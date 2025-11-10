d625b5d2875fda1168a6afc128a97a6cfdbb32da
rafi-arzupinar <rafiarzupinar@gmail.com>
2025-11-10 12:33:45 +0300

    title değişti

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import './globals.css'
 
 export const metadata = {
-  title: 'Mutlu Başlangıç',
+  title: 'MutluBasla',
   description: 'Kolaylaştırın güçleştirmeyin müjdeleyin nefret ettirmeyim(s.a.v)',
 }
 
