066b0fcbd794dc15df90131a3968c34b560be106
rafi-arzupinar <rafiarzupinar@gmail.com>
2025-11-10 12:20:44 +0300

    title değişti

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import './globals.css'
 
 export const metadata = {
-  title: 'Next.js MongoDB Template',
+  title: 'Mutlu Başlangıç',
   description: 'A simple template with App Router, MongoDB, and shadcn/ui',
 }
 
